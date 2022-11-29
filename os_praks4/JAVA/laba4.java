6b8190b25bb7d5244526beedc8aeda1562f063e8
SL4SH73R <87204613+sl4sh73r@users.noreply.github.com>
2022-11-29 04:04:54 +0300

    Update laba4.java

--- os_praks4/JAVA/laba4.java
+++ os_praks4/JAVA/laba4.java
@@ -6,12 +6,15 @@ public class Main {
         int numIterrations = 100000000;
         long m;
         for (int j = 0; j < 5; j++) {
-            int a = 0, b = 3, c = 3;
+            int a = 0, b = 2, c = 3;
             m = System.currentTimeMillis();
             for (int i = 0; i < numIterrations; i++) {
                 a += b * 2 + c - i;
             }
+            long unsigned = Integer.toUnsignedLong(a);
+            System.out.println(unsigned);
             System.out.println((double) (System.currentTimeMillis() - m) * 1.000 + " ms");
+
         }
     }
 }
